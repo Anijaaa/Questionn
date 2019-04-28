136f1ca29b8f66879757bc8bc53bc4c4737fed96
Anijaaa <40270318+Anijaaa@users.noreply.github.com>
2019-04-28 16:39:03 +0900

    Update question.py

--- question.py
+++ question.py
@@ -42,13 +42,6 @@ def predicate(message,l,r):
 # -------------------------------------------------------------------------------------------------------------------
 @client.event
 async def on_ready():
-    counter = 0
-    all_message = "571660933915607040"
-    channel_name = client.get_channel(all_message)
-    async for log in client.logs_from(channel_name.server.channels,limit=9999999):
-        if log.server.id == "571513405920510004":
-            counter += 1
-    await client.edit_channel(channel_name,name="MESSAGE COUNT : {}".format(counter))
     async for log in client.logs_from(client.get_channel("571521713121001483"),limit=100):
         if log.channel.id == "571521713121001483":
             await client.delete_message(log)
@@ -103,6 +96,21 @@ async def on_member_remove(member):
 
 @client.event
 async def on_message(message):
+    if datetime.now().strftime("%H:%M:%S") == datetime.now().strftime(
+            "12:00:00") or message.content == "<update-message":
+        if message.author.server_permissions.administrator:
+            if not message.server.id == "521143812278714378":
+                return
+            await client.delete_message(message)
+            counter = 0
+            all_message = "569835558642384896"
+            channel_name = client.get_channel(all_message)
+            for i in message.server.channels:
+                async for log in client.logs_from(i,limit=99999999999):
+                    if log.server.id == message.server.id:
+                        counter += 1
+            await client.edit_channel(channel_name,name="メッセージカウント｜MESSAGE COUNT : {}".format(counter))
+            return
     help_message = ["""
             -------------------------------
             This BOT was produced by Mr.𝗠𝗞𝗠𝗞𝟭𝟭𝟬𝟭™#3577
